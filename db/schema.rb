d444413f88fec7c0e79e3fbc5c04201d01c6875f
Yu-Jye Tung <yu.jye.tung@gmail.com>
2014-01-24 21:41:10 +0000

    Add contact and company models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140117202517) do
+ActiveRecord::Schema.define(version: 20140124212810) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -45,6 +45,24 @@ ActiveRecord::Schema.define(version: 20140117202517) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+
+  create_table "companies", force: true do |t|
+    t.string   "name"
+    t.string   "industry"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
+    t.integer  "company_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "contacts", ["company_id"], name: "index_contacts_on_company_id"
 
   create_table "events", force: true do |t|
     t.string   "name"
